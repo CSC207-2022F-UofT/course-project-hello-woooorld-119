c492e2950767fbac4b6e2e15edd7a42935e0d636
is1006 <Is@B3ls#1n1>
2022-11-16 16:20:39 -0500

    New User class (with id)

--- src/main/java/tutorial/User.java
+++ src/main/java/tutorial/User.java
@@ -1,13 +1,19 @@
 package tutorial;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
+import java.sql.Timestamp;
+
 public class User {
     private String displayName;
     private final int id;
     private ArrayList<String> listofChatroom;
     private ArrayList<String> friendsList;
     private String username;
+    private String password;
+    private ArrayList<Timestamp> loginTracker;
 
     /**
      * Construct a User
@@ -17,12 +23,14 @@ public class User {
      * @param username the User's username only accessible by the User themself
      */
 
-    public User(String displayName, int id, String username) {
+    public User(String displayName, int id, String username, String password) {
         this.displayName = displayName;
         this.id = id; //need to determine how we will be creating id and ensuring it does not already exist //
         this.username = username;
         this.listofChatroom = new ArrayList<String>();
         this.friendsList = new ArrayList<String>();
+        this.password = password;
+        this.loginTracker = new ArrayList<>();
     }
 
     public void addUserToChatroom(String chatroom_name){
@@ -38,6 +46,10 @@ public class User {
         this.friendsList.remove(friend_username);
     }
 
+    public void setPassword(String password){
+        this.password = password;
+    }
+
     public String getUsername(){
         return this.username;
     }
@@ -48,5 +60,21 @@ public class User {
 
     public ArrayList<String> getFriendsList(){
         return this.friendsList;
+    }
+
+    // Setter of loginTracker and getter of loginTacker
+
+    public void setLoginTracker(ArrayList<Timestamp>loginTracker){
+        this.loginTracker = loginTracker;
+    }
+
+    public ArrayList<Timestamp>getLoginTracker(){
+        return loginTracker;
+    }
+
+    // Method to update login tracker
+
+    public void updateLoginTracker(Timestamp timing){
+        loginTracker.add(timing);
     }
 }
