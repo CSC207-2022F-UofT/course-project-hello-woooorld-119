65a3acff3c70020938c3857f117cec3dc93db7d8
jeffhu <jeff1hu@gmail.com>
2022-11-28 17:09:29 -0500

    rearrange packages

--- src/main/java/Storage/Accounts/AccountsStorageUseCase.java
+++ src/main/java/Storage/Accounts/AccountsStorageUseCase.java
@@ -8,17 +8,17 @@ import java.util.Objects;
 import Storage.Reader;
 
 public class AccountsStorageUseCase implements AccountsStorageGateway {
-    Storage accounts_storage;
+    Storage accountsStorage;
     Reader reader;
 
     public AccountsStorageUseCase() {
-        this.accounts_storage = new AccountsStorage();
+        this.accountsStorage = new AccountsStorage();
         this.reader = new Reader();
     }
 
     @Override
     public boolean usernameExists(String username) {
-        Map<String, String> info = (Map<String, String>) this.reader.Read(accounts_storage.readfile(), Map.class);
+        Map<String, String> info = (Map<String, String>) this.reader.Read(accountsStorage.readfile(), Map.class);
 
         return info.containsKey(username);
     }
@@ -26,15 +26,15 @@ public class AccountsStorageUseCase implements AccountsStorageGateway {
     @Override
     public boolean passwordExists(String username, String password) {
         // username must exist
-        Map<String, String> info = (Map<String, String>) this.reader.Read(accounts_storage.readfile(), Map.class);
+        Map<String, String> info = (Map<String, String>) this.reader.Read(accountsStorage.readfile(), Map.class);
         return Objects.equals(info.get(username), password);
     }
 
     @Override
     public void save(String username, String password) {
-        Map<String, String> map = (Map<String, String>) reader.Read(accounts_storage.readfile(), Map.class);
+        Map<String, String> map = (Map<String, String>) reader.Read(accountsStorage.readfile(), Map.class);
         map.put(username, password);
 
-        accounts_storage.writefile(reader.ToString(map));
+        accountsStorage.writefile(reader.ToString(map));
     }
 }
