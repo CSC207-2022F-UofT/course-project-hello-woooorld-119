ef556823742965f9550ec61c6e34a7b1b7be3e17
is1006 <Is@B3ls#1n1>
2022-11-28 17:03:55 -0500

    Added Test Classes to the Test Folder

--- src/test/java/TestCases/UserTests.java
+++ src/test/java/TestCases/UserTests.java
@@ -3,11 +3,7 @@ package TestCases;
 import tutorial.User;
 
 public class UserTests {
-    User martin = new User("MARS", "martin123", "password321");
-    Chatroom
 
-    @Test
-    public void testaddUserToChatroom("")
 
 
 
