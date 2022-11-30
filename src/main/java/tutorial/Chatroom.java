78a16439f53a0cda16eb26d0cfac667830804a5a
nivemadhi <nivedha.madhivanan@mail.utoronto.ca>
2022-11-30 15:40:46 -0500

    Updated the Chatroom class with the following changes: - the users and admin are now stored as the user objects themselves rather than the previous basis where they were stored based on username

--- src/main/java/tutorial/Chatroom.java
+++ src/main/java/tutorial/Chatroom.java
@@ -7,29 +7,29 @@ import java.util.ArrayList;
 
 public class Chatroom {
     private String name;
-    private ArrayList<String> user_list; // list of unique usernames of the users in the chatroom
-    private String admin_name; // User who created the chatroom
+    private ArrayList<User> userList; // list of unique usernames of the users in the chatroom
+    private User admin; // User who created the chatroom
 
-    private ArrayList<Message> message_list; // list of messages in the chatroom
+    private ArrayList<Message> messageList; // list of messages in the chatroom
 
     private ChatroomStorageGateway gateway; // interface to save chatroom
-    public Chatroom(String name, String admin_name, ChatroomStorageGateway gateway) {
+    public Chatroom(String name, User admin, ChatroomStorageGateway gateway) {
         this.name = name;
-        this.user_list = new ArrayList<>();
-        this.admin_name = admin_name;
-        this.user_list.add(admin_name);
-        this.message_list = new ArrayList<>();
+        this.userList = new ArrayList<>();
+        this.admin = admin;
+        this.userList.add(admin);
+        this.messageList = new ArrayList<>();
         this.gateway = gateway;
         this.gateway.saveData(this);
     }
 
-    public void AddUser(String username){
-        this.user_list.add(username);
+    public void AddUser(User newUser){
+        this.userList.add(newUser);
         this.gateway.saveData(this);
     }
 
-    public void RemoveUser(String username){
-        this.user_list.remove(username);
+    public void RemoveUser(User toBeRemoved){
+        this.userList.remove(toBeRemoved);
         this.gateway.saveData(this);
     }
 
@@ -40,17 +40,17 @@ public class Chatroom {
         this.name = new_name;
         this.gateway.saveData(this);
     }
-    public String getAdmin_name (){
-        return this.admin_name;
+    public User getAdmin(){
+        return this.admin;
     }
 
-    public void setAdmin(String new_admin_name){
-        this.admin_name = new_admin_name;
+    public void setAdmin(User newAdmin){
+        this.admin = newAdmin;
         this.gateway.saveData(this);
     }
 
     public void addMessage(Message message_to_add){
-        this.message_list.add(message_to_add);
+        this.messageList.add(message_to_add);
         this.gateway.saveData(this);
     }
 
