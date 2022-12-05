c3dea7afbdf973e4e8df184f60a4fa260ccbbc25
Samandar <samandar.vijay@mail.utoronto.ca>
2022-12-05 15:50:34 -0500

    December 5, 2022 Created test cases for the login ui, register ui, private profile page ui, and public profile page. Tested the overall functionality of the login system and profile editing system. Checked whether the classes were properly engaging with each other by following the clean architecture.

---
+++ src/main/java/tutorial/EditProfileStrategyInterface.java
@@ -0,0 +1,10 @@
+package tutorial;
+
+public interface EditProfileStrategyInterface {
+    //Designed to be implemented by classes that edit profile pages for users.
+
+    //This method is meant to be implemented by classes that modify profile pages.
+    // Displays the UI of the profile page.
+    public void display();
+
+}
